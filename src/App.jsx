0332660086e11ea37dba2b2238360e7a1a9e40da
Medardo Cuellar <soyriku@gmail.com>
2024-06-18 22:11:49 -0500

    se agrega editar y eliminar todo

--- src/App.jsx
+++ src/App.jsx
@@ -9,16 +9,34 @@ export default function App() {
     setTodos([...todos, text]);
     setText(""); //limpia el input
   }
-  function dentroSubmit(event) {
+  function alEnviar(event) {
     event.preventDefault();
     addTodo(); // se puede quitar el onClick porque el onsubmit lo va a ejecutar
+  }
+
+  function removeTodo(indexToRemove) {
+    /*     
+    todos.splice(indexToRemove, 1);
+    setTodos([...todos]); */
+    const newTodos = todos.filter((todo, index) => index !== indexToRemove);
+    setTodos(newTodos);
+  }
+
+  function editTodo(index) {
+    const newTodos = todos.map((todo, i) => {
+      if (i === index) {
+        return prompt("Edita tu tarea", todo);
+      }
+      return todo;
+    });
+    setTodos(newTodos);
   }
 
   return (
     <main className="w-full min-h-screen">
       <form
         className="flex flex-row gap-2 justify-center p-5"
-        onSubmit={dentroSubmit}
+        onSubmit={alEnviar}
       >
         <input
           type="text"
@@ -26,6 +44,7 @@ export default function App() {
           placeholder="Escribe tu tarea"
           value={text}
           onChange={(event) => setText(event.target.value)}
+          required
         />
         <button
           className="bg-white text-black px-3 rounded" /* onClick={addTodo} se quita porque ahora con el submit se pone*/
@@ -33,9 +52,33 @@ export default function App() {
           + Agregar
         </button>
       </form>
-      <div className="text-white">
+      <div className="max-w-screen-sm w-full mx-auto p-4">
+        {todos.length === 0 && (
+          <p className="bg-white/50 rounded p-4 m-4 flex flex-row justify-center">
+            no tienes tareas pendientes 🤷‍♀️
+          </p>
+        )}
         {todos.map((todo, index) => {
-          return <div key={`todo-${index}`}>{todo}</div>;
+          return (
+            <div
+              key={`todo-${index}`}
+              className="bg-white/10 rounded p-4 m-4 flex flex-row gap-4 justify-between"
+            >
+              <span className="flex-grow p-4">{todo}</span>
+              <span
+                className="cursor-pointer p-4 hover:rounded-full hover:bg-green-500/50"
+                onClick={() => editTodo(index)}
+              >
+                📝
+              </span>
+              <span
+                className="cursor-pointer p-4 hover:rounded-full hover:bg-red-500/50"
+                onClick={() => removeTodo(index)}
+              >
+                ❌
+              </span>
+            </div>
+          );
         })}
       </div>
     </main>
